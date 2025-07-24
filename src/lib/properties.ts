486a46e78630f58e45e929713bf4f91b33910aa8
Folixx Ai <folixxai@gmail.com>
2025-07-24 19:29:21 +0000

    fix

--- src/lib/properties.ts
+++ src/lib/properties.ts
@@ -166,7 +166,6 @@ export const properties = [
         "https://res.cloudinary.com/dbczzmftw/image/upload/v1753056247/n0nz8lf2pkgnhrvk96r7.jpg",
         "https://res.cloudinary.com/dbczzmftw/image/upload/v1753056265/n39hafncv0hvahjzvz.jpg",
         "https://res.cloudinary.com/dbczzmftw/image/upload/v1753056216/iuoevz8kfmhn4hosx9i0.jpg",
-        "https://res.cloudinary.com/dbczzmftw/image/upload/v1753056247/n0nz8lf2pkgnhrvk96r7.jpg",
         "https://res.cloudinary.com/dbczzmftw/image/upload/v1753056265/n39hafncv0hvahjzvz.jpg",
         "https://res.cloudinary.com/dbczzmftw/image/upload/v1753056289/uu92ucmkyuct93tuzcrh.jpg",
         "https://res.cloudinary.com/dbczzmftw/image/upload/v1753056307/g0ulm2ucr0cug3jl5ldq.jpg"
