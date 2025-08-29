bbf10f1371eb3b76736af6bf468a8ef8e93e38cf
MAC <mac@MACs-MacBook-Pro.local>
2025-08-29 17:17:51 +0100

    Updated book inspection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,3 +25,15 @@ export default function Home() {
     </div>
   );
 }
+
+<form name="contact" netlify>
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
