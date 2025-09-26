8fae19fd51f9c2a7b106b03717204f96ce547548
MAC <mac@MACs-MacBook-Pro.local>
2025-09-26 18:26:59 +0100

    Removed the download brochure button

--- src/lib/properties.ts
+++ src/lib/properties.ts
@@ -291,7 +291,7 @@ export const properties = [
       "carSpace": "Ground & First Floor Dedicated Parking",
       "typology": "2-Bedroom, 3-Bedroom, 4-Bedroom Maisonettes, 5-Bedroom Penthouse",
       "saleStatus": "Limited Units Available",
-      "brochureUrl": "https://drive.google.com/file/d/1y8H5Bhpx7QcFNC2cAU6Ji2L6L72qYwK-/view?usp=sharing"
+      "brochureUrl": "https://www.dropbox.com/scl/fi/j898rw5m6zchs50ixgjmq/Elysian-Brochure-New.pdf?rlkey=g6b579aakpwmoa3exx0u5fzrw&st=z821cvff&dl=0"
     },
     "description": "Inspired by the ideology of heroism and the iconic craftsmanship of the Rolls-Royce Boat Tail, The Elysian Rise is a bold expression of opulence, intelligence, and timeless architecture. Located at Musa Ya’dua Street, Victoria Island, this flagship 14-floor high-rise redefines luxury living at the intersection of elegance and innovation.",
     "availableUnits": [
