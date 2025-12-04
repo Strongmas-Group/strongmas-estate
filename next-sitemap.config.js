46bfd00040d6c5ab431fb68d245a7be83589eca2
MAC <mac@MACs-MacBook-Pro.local>
2025-12-04 16:34:11 +0100

    Updated the  sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://https://strongmasresidence.com', 
+    siteUrl: 'https://strongmasresidence.com', 
     generateRobotsTxt: true,        
     sitemapSize: 7000,
   
