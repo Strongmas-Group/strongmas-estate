6f14cd9420cebd64ac99e3d08b2f3a713cde28fb
MAC <mac@MACs-MacBook-Pro.local>
2026-01-12 12:59:43 +0100

    Added a chatbot

--- .next/dev/server/chunks/ssr/[root-of-the-server]__93652d3f._.js
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__93652d3f._.js
@@ -917,6 +917,48 @@ function RootLayout({ children }) {
                         fileName: "[project]/src/app/layout.tsx",
                         lineNumber: 132,
                         columnNumber: 9
+                    }, this),
+                    /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$script$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["default"], {
+                        id: "livechat-widget",
+                        strategy: "afterInteractive",
+                        children: `
+            window.__lc = window.__lc || {};
+            window.__lc.license = 19449911;
+            window.__lc.integration_name = "manual_onboarding";
+            window.__lc.product_name = "livechat";
+
+            ;(function(n,t,c){
+              function i(n){
+                return e._h ? e._h.apply(null,n) : e._q.push(n)
+              }
+              var e={
+                _q:[],
+                _h:null,
+                _v:"2.0",
+                on:function(){i(["on",c.call(arguments)])},
+                once:function(){i(["once",c.call(arguments)])},
+                off:function(){i(["off",c.call(arguments)])},
+                get:function(){
+                  if(!e._h) throw new Error("[LiveChatWidget] You can't use getters before load.");
+                  return i(["get",c.call(arguments)])
+                },
+                call:function(){i(["call",c.call(arguments)])},
+                init:function(){
+                  var n=t.createElement("script");
+                  n.async=true;
+                  n.type="text/javascript";
+                  n.src="https://cdn.livechatinc.com/tracking.js";
+                  t.head.appendChild(n);
+                }
+              };
+              !n.__lc.asyncInit && e.init();
+              n.LiveChatWidget = n.LiveChatWidget || e;
+            }(window, document, [].slice));
+          `
+                    }, void 0, false, {
+                        fileName: "[project]/src/app/layout.tsx",
+                        lineNumber: 135,
+                        columnNumber: 17
                     }, this)
                 ]
             }, void 0, true, {
